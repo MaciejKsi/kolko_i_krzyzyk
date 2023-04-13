1e003d58198ad6a1ee8a55a41d2dfa9c69212293
MaciejKsi <125036778+MaciejKsi@users.noreply.github.com>
2023-04-13 16:04:49 +0200

    ez

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ function znak(id) {
 
   console.log(plansza);
   czyWygrana();
+  generujPlansze();
 }
 
 function czyWygrana() {
@@ -42,10 +43,55 @@ function czyWygrana() {
       blokujPlansze();
     }
   }
+  for (let i = 0; i <= 2; i++) {
+    if (
+      plansza[0][i] == plansza[1][i] &&
+      plansza[0][i] == plansza[2][i] &&
+      plansza[0][i] != undefined
+    ) {
+      console.log("Wygrana");
+      blokujPlansze();
+    }
+  }
+
+  for (let i = 0; i <= 2; i++) {
+    if (
+      plansza[0][0] == plansza[1][1] &&
+      plansza[0][0] == plansza[2][2] &&
+      plansza[0][0] != undefined
+    ) {
+      console.log("Wygrana");
+      blokujPlansze();
+    }
+  }
+  for (let i = 0; i <= 2; i++) {
+    if (
+      plansza[0][2] == plansza[1][1] &&
+      plansza[1][1] == plansza[2][0] &&
+      plansza[2][0] != undefined
+    ) {
+      console.log("Wygrana");
+      blokujPlansze();
+    }
+  }
 }
 
 function blokujPlansze() {
   for (let i = 1; i <= 9; i++) {
     document.getElementById(i.toString()).removeAttribute("onclick");
+  }
+}
+
+function generujPlansze() {
+  var licznik2 = 1;
+
+  for (let i = 0; i <= 2; i++) {
+    for (let j = 0; j <= 2; j++) {
+      console.log(plansza[i][j]);
+      if (plansza[i][j] != undefined) {
+        document.getElementById(licznik2.toString()).innerHTML = plansza[i][j];
+      }
+      licznik2++;
+    }
   }
 }
